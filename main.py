39d944e523ae9abcc329cc1388a49df6d5ff55c1
ShivamChawla01 <33668025+ShivamChawla01@users.noreply.github.com>
2018-06-19 23:04:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -162,7 +162,9 @@ def run():
         correct_label=tf.placeholder(tf.int32,[None,None,None,num_classes],name='correct_label')
         learning_rate=tf.placeholder(tf.float32,name='learning_rate')
 
-        nn_last_layer=layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        image,keep_prob,layer_3_output,layer_4_output,layer_7_output=load_vgg(sess,vgg_path)
+
+        nn_last_layer=layers(layer_3_output, layer_4_output, layer_7_output, num_classes)
 
         logits,train_op,cross_entropy_loss=optimize(nn_last_layer, correct_label, learning_rate, num_classes)
 
